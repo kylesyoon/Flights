353bd8e46b4478301d14eab8fa0babea1c7f72ac
Kyle Yoon <apps.yoon@gmail.com>
2016-02-11 20:43:09 -0600

    Hitting the API

---
+++ Flights/Flight.swift
@@ -0,0 +1,13 @@
+//
+//  Flight.swift
+//  Flights
+//
+//  Created by Kyle Yoon on 2/11/16.
+//  Copyright © 2016 Kyle Yoon. All rights reserved.
+//
+
+import Foundation
+
+struct Flight {
+    
+}
